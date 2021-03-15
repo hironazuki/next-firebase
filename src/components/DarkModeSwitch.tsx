11565b53b6ec7b147c8035a68c1f2dde25c413bb
hironakazuki <far_east_nightbird30@icloud.com>
2021-03-15 16:42:21 +0900

    chore: DarkModeSwitchの変更 react-toggle-dark-modeのスイッチを使用

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -1,16 +1,7 @@
 import { useColorMode, Switch } from '@chakra-ui/react';
-
+import { DarkModeSwitch as ReactDarkModeSwitch } from 'react-toggle-dark-mode';
 export const DarkModeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === 'dark';
-  return (
-    <Switch
-      // position="fixed"
-      top="1rem"
-      right="1rem"
-      color="green"
-      isChecked={isDark}
-      onChange={toggleColorMode}
-    />
-  );
+  return <ReactDarkModeSwitch checked={isDark} onChange={toggleColorMode} size={20} />;
 };
